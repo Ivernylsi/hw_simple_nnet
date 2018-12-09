da284607fa5838bd5e3e0d24e6f5067fc4227005
dsinitsyn <dsinitsyn@gmail.com>
2018-12-09 22:19:15 +0300

    random init

--- main.cpp
+++ main.cpp
@@ -27,7 +27,8 @@ Eigen::MatrixXd getLabels() {
 }
 
 Eigen::MatrixXd getTrainData() {
-  return read_Mnist("/home/little/ML/Nnetwork/data/train-images-idx3-ubyte");
+  return (read_Mnist("/home/little/ML/Nnetwork/data/train-images-idx3-ubyte") /
+          255.0);
 }
 
 auto interpretAns(const Eigen::MatrixXd &v) {
@@ -52,13 +53,14 @@ int main() {
   std::cout << "Dataset reading completed.\n";
 
   const int size = 1000;
+  double ans;
+  for (int k = 0; k < 100; ++k) {
+    for (int i = 0; i < 10; ++i) {
 
-  for(int k = 0; k < 10; ++k)
-  for (int i = 0; i < 10; ++i) {
-
-    double ans = net.training<loss::CrossEntropySoftMax>(X.block(size * i, 0, size * (i + 1), 784),
-                                     y.block(size * i, 0, size * (i + 1), 10),
-                                     10);
+      ans = net.training<loss::CrossEntropySoftMax>(
+          X.block(size * i, 0, size * (i + 1), 784),
+          y.block(size * i, 0, size * (i + 1), 10), 1);
+    }
     std::cout << k << " Loss os " << ans << std::endl;
   }
   return 0;
--- n_network/include/n_network/Layers.hpp
+++ n_network/include/n_network/Layers.hpp
@@ -1,9 +1,16 @@
 #ifndef LAYERS_HPP
 #define LAYERS_HPP
 #include <Eigen/Eigen>
-#include <random>
 #include <iostream>
+#include <random>
 
+namespace {
+double getNormal(const double &) {
+  std::normal_distribution<double> runif(-1.0, 1.0);
+  std::mt19937 rng;
+  return runif(rng);
+}
+} // namespace
 
 template <size_t N> struct Input {
   static const size_t InN = N;
@@ -18,10 +25,10 @@ template <size_t N, size_t K> struct Dense {
   static const size_t InN = N;
   static const size_t OutN = K;
 
-  Dense() { 
-    w = Eigen::MatrixXd(InN, OutN);
-    w.setOnes();
-    w/=100000; }
+  Dense() {
+    auto kek = [](auto &a) { return getNormal(a);};
+    w = Eigen::MatrixXd(InN, OutN).unaryExpr(kek);
+  }
 
   Eigen::MatrixXd forward(const Eigen::MatrixXd &in) {
     input = in;
@@ -30,8 +37,8 @@ template <size_t N, size_t K> struct Dense {
 
   Eigen::MatrixXd backward(const Eigen::MatrixXd &in) {
     Eigen::MatrixXd grad = input.transpose() * in;
-    input.resize(0,0); 
-    w -= 0.0001 * grad ;
+    input.resize(0, 0);
+    w -= 0.1 * grad;
     return grad;
   }
 
@@ -44,9 +51,7 @@ template <size_t N> struct Output {
   static const size_t InN = N;
   static const size_t OutN = N;
 
-  Eigen::MatrixXd forward(const Eigen::MatrixXd &in) {
-    return in;
-  }
+  Eigen::MatrixXd forward(const Eigen::MatrixXd &in) { return in; }
 
   Eigen::MatrixXd backward(const Eigen::MatrixXd &in) {
     return in * Eigen::VectorXd::Ones(InN);
